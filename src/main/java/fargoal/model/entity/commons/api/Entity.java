16bffb53d5e021d9244e43d16d55fcf75487bb2b
Daniele <daniele.tramonti2@studio.unibo.it>
2025-01-31 11:33:47 +0100

    fixed gerarchy of interfaces

--- src/main/java/fargoal/model/entity/commons/api/Entity.java
+++ src/main/java/fargoal/model/entity/commons/api/Entity.java
@@ -3,7 +3,7 @@ package fargoal.model.entity.commons.api;
 import fargoal.api.Position;
 import fargoal.model.FloorElement;
 
-public interface Entity {
+public interface Entity extends FloorElement{
     
     public Integer getHealth();
 
