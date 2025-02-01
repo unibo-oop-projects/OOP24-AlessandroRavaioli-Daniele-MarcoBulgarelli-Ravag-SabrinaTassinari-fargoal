6c391f60c19d2db07f82e92eb262662aee8060d7
Daniele <daniele.tramonti2@studio.unibo.it>
2025-02-01 10:22:43 +0100

    fixed some Javadocs comments

--- src/main/java/fargoal/model/entity/commons/api/Health.java
+++ src/main/java/fargoal/model/entity/commons/api/Health.java
@@ -2,25 +2,25 @@ package fargoal.model.entity.commons.api;
 
 public interface Health {
     
-    /*
+    /**
      * Getter for the current health.
      * @return current health.
      */
     public Integer getCurrentHealth();
 
-    /*
+    /**
      * Getter for the max health of the floor.
      * @return max health
      */
     public Integer getMaxHealth();
 
-    /*
+    /**
      * Decreases the current health value.
      * @param amount to subtract to the current health value.
      */
     public void decreaseHealth(Integer amount);
 
-    /*
+    /**
      * Increases the current health value.
      * @param amount to add to the current health value.
      */
