900d3d2558f21b65ffea2050525cf972118e402c
Daniele <daniele.tramonti2@studio.unibo.it>
2025-02-02 21:24:23 +0100

    implemented class Ai

--- src/main/java/fargoal/model/entity/monsters/ai/Ai.java
+++ src/main/java/fargoal/model/entity/monsters/ai/Ai.java
@@ -8,36 +8,64 @@ import fargoal.model.entity.player.api.Player;
 public class Ai {
 
     private final static Integer POSSIBLE_DIRECTIONS = 2;
+    private final static Integer MAX_DISTANCE = 10;
 
     public static void move(AbstractMonster monster, Player player) {
+        Integer xDistance = Math.abs(monster.getPosition().x() - player.getPosition().x());
+        Integer yDistance = Math.abs(monster.getPosition().y() - player.getPosition().y());
+        boolean xMonsterBigger = false;
+        boolean yMonsterBigger = false;
         Position pos;
-        if(monster.getPosition().x() - player.getPosition().x() < 10
-                && monster.getPosition().y() - player.getPosition().y() < 10) {
-                    //nel caso sia piu lontano nelle ascisse
-                    if(Math.abs(monster.getPosition().x() - player.getPosition().x()) >= Math.abs(monster.getPosition().y() - player.getPosition().y())) {
-                        if(monster.getPosition().x() > player.getPosition().x()) {
-                            pos = monster.getPosition().increaseX();
-                            if(monster.getFloorMap().isTile(pos)) {
-                                monster.setPosition(pos);
-                            }
-                        } else {
-                            pos = monster.getPosition().decreaseX();
-                            if(monster.getFloorMap().isTile(pos)) {
-                                monster.setPosition(pos);
-                            }
-                        }
-                    //se invece è piu lontano nelle ordinate
-                    } else {
-                        
+        
+        if(monster.getPosition().x() >= player.getPosition().x()) {
+            xMonsterBigger = true;
+        }
+        if(monster.getPosition().y() >= player.getPosition().y()) {
+            yMonsterBigger = true;
+        }
+        //controllo se il mostro vede il player
+        if(xDistance < MAX_DISTANCE && yDistance < MAX_DISTANCE) {
+            //nel caso sia piu lontano nelle ascisse
+            if(xDistance >= yDistance) {
+                //controllo se l'ascissa del mostro è maggiore di quella del player
+                if(xMonsterBigger) {
+                    pos = monster.getPosition().decreaseX();
+                    if(monster.getFloorMap().isTile(pos)) {
+                        monster.setPosition(pos);
                     }
-
-
+                //se invece è minore
                 } else {
-                    do {
-                        pos = monster.getPosition().add(new Position(monster.getRandom(POSSIBLE_DIRECTIONS), monster.getRandom(POSSIBLE_DIRECTIONS)));
-                    } while (monster.getFloorMap().isTile(pos));
-                    monster.setPosition(pos);
+                    pos = monster.getPosition().increaseX();
+                    if(monster.getFloorMap().isTile(pos)) {
+                        monster.setPosition(pos);
+                    }
                 }
+            //se invece è piu lontano nelle ordinate
+            } else {
+                //controllo se l'ordinata del mostro è maggiore di quella del player
+                if(yMonsterBigger) {
+                    pos = monster.getPosition().decreaseY();
+                    if(monster.getFloorMap().isTile(pos)) {
+                        monster.setPosition(pos);
+                    }
+                //se invece è minore
+                } else {
+                    pos = monster.getPosition().increaseY();
+                    if(monster.getFloorMap().isTile(pos)) {
+                        monster.setPosition(pos);
+                    } else {
+
+                    }
+                }
+            }
+
+
+        } else {
+            do {
+                pos = monster.getPosition().add(new Position(monster.getRandom(POSSIBLE_DIRECTIONS), monster.getRandom(POSSIBLE_DIRECTIONS)));
+            } while (monster.getFloorMap().isTile(pos));
+            monster.setPosition(pos);
+        }
     }
 
     public static void moveSpider(Spider spider, Player player) {
