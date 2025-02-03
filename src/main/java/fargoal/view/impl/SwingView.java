420cd4614e8758ce3e9d0e629a24afd76c4683dd
Alessandro Ravaioli <alessandro.ravaioli8@studio.unibo.it>
2025-02-03 09:45:59 +0100

    removed a useless test and modified SwingView class

--- src/main/java/fargoal/view/impl/SwingView.java
+++ src/main/java/fargoal/view/impl/SwingView.java
@@ -1,8 +1,7 @@
 package fargoal.view.impl;
 
+import java.awt.BorderLayout;
 import java.awt.Graphics2D;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.function.Consumer;
 import javax.swing.WindowConstants;
 
@@ -18,27 +17,16 @@ public class SwingView implements View {
     private SwingViewCanvas canvas;
     private JFrame frame;
 
-    private int truePixelHeight;
-    private int truePixelWidth;
     private int tilePixelDim;
 
     public SwingView() {
         this.frame = new JFrame();
         this.canvas = new SwingViewCanvas();
-        this.frame.getContentPane().add(canvas);
+        this.frame.setLayout(new BorderLayout());
+        this.frame.getContentPane().add(canvas, BorderLayout.CENTER);
         this.frame.setResizable(true);
-        this.truePixelHeight = this.frame.getBounds().height;
-        this.truePixelWidth = this.frame.getBounds().width;
-        this.tilePixelDim = Math.min(this.truePixelHeight/FLOOR_HEIGTH, this.truePixelWidth/FLOOR_LENGTH);
         this.frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.frame.addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent ev){
-				System.exit(-1);
-			}
-			public void windowClosed(WindowEvent ev){
-				System.exit(-1);
-			}
-        });
+        calculateDimensions();
         this.frame.setVisible(true);
     }
 
@@ -48,6 +36,7 @@ public class SwingView implements View {
 
     @Override
     public void update() {
+        calculateDimensions();
         this.canvas.repaint();
     }
     
@@ -56,11 +45,15 @@ public class SwingView implements View {
         this.canvas.addToList(g2d);
     }
 
-    public int getHeight() {
-        return this.frame.getBounds().height;
+    public int getMapHeight() {
+        return this.canvas.getBounds().height;
     }
 
-    public int getWidth() {
-        return this.frame.getBounds().width;
+    public int getMapWidth() {
+        return this.canvas.getBounds().width;
+    }
+
+    private void calculateDimensions() {
+        this.tilePixelDim = Math.min(this.getMapHeight()/FLOOR_HEIGTH, this.getMapWidth()/FLOOR_LENGTH);
     }
 }
