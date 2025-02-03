d9e54e219ce2cb7458a35166113a24460b5bd374
Alessandro Ravaioli <alessandro.ravaioli8@studio.unibo.it>
2025-02-03 09:50:02 +0100

    updated GameEngine

--- src/main/java/fargoal/model/core/GameEngine.java
+++ src/main/java/fargoal/model/core/GameEngine.java
@@ -2,24 +2,27 @@ package fargoal.model.core;
 
 import fargoal.model.manager.api.FloorManager;
 import fargoal.model.manager.impl.FloorManagerImpl;
+import fargoal.view.api.View;
+import fargoal.view.impl.SwingView;
 
 public class GameEngine {
 
     private static final int period = 20; 
     private final FloorManager manager;
+    private final View view;
 
     public GameEngine() {
         this.manager = new FloorManagerImpl();
+        this.view = new SwingView();
     }
     
     public void start() {
-        long previousCycleStartTime = System.currentTimeMillis();
         while (true) {
             long currentCycleStartTime = System.currentTimeMillis();
             manager.update(null);
+            view.update();
             waitToNextFrame(currentCycleStartTime);
             System.out.println("Sono partito");
-            previousCycleStartTime = currentCycleStartTime;
             System.exit(0);
         }
     }
