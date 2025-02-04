c174fa2716c91cf3454bb9a22a498e8eda4b98a3
Marco Bulgarelli <marco.bulgarelli6@studio.unibo.it>
2025-02-04 21:23:16 +0100

    Added Gold interface and it implementation

---
+++ src/main/java/fargoal/model/entity/player/api/Gold.java
@@ -0,0 +1,30 @@
+package fargoal.model.entity.player.api;
+
+/**
+ * Interface to manage gold.
+ */
+
+public interface Gold {
+    /**
+     * Getter for current gold amount.
+     * 
+     * @return the current gold amount.
+     */
+    Integer getCurrentGold();
+
+    /**
+     * Sets current gold amount to 0.
+     */
+    void resetGold();
+
+    /**
+     * Increases the current gold amount.
+     * If the amount to add exceeds the max capacity,
+     * only the amount needed to reach the limit is added
+     * and the leftover gold is returned.
+     * 
+     * @param amount - the amount of gold to add.
+     * @return the leftover gold amount that could not be added.
+     */
+    Integer addGold(Integer amount);
+}
---
+++ src/main/java/fargoal/model/entity/player/impl/GoldImpl.java
@@ -0,0 +1,53 @@
+package fargoal.model.entity.player.impl;
+
+import fargoal.model.entity.player.api.Gold;
+
+/**
+ * Gold class, it manages player's gold.
+ */
+public class GoldImpl implements Gold{
+
+    private static final int INITIAL_GOLD_MAX_CAPACITY = 100;
+    private Integer currentGold;
+    private Integer maxCapacity;
+
+    /**
+     * Creates an instance of Gold, the initial gold is 
+     * set to 0.
+     */
+    public GoldImpl(){
+        this.currentGold = 0;
+        this.maxCapacity = INITIAL_GOLD_MAX_CAPACITY;
+    }
+
+    /**{@inheritDoc} */
+    @Override
+    public Integer getCurrentGold() {
+        return this.currentGold;
+    }
+
+    public Integer getMaxCapacity() {
+        return this.maxCapacity;
+    }
+
+    /**{@inheritDoc} */
+    @Override
+    public Integer addGold(Integer amount) {
+        if(amount < 0) {
+            throw new IllegalArgumentException("You cannot add a negative amount");
+        }
+
+        int spaceAvailable = this.getMaxCapacity() - this.getCurrentGold();
+        int goldToAdd = Math.min(amount, spaceAvailable);
+        this.currentGold = this.currentGold + goldToAdd;
+
+        return amount - goldToAdd;
+    }
+
+    /**{@inheritDoc} */
+    @Override
+    public void resetGold() {
+        this.currentGold = 0;
+    }
+    
+}
