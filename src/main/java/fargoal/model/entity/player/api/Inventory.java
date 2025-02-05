b53739d849d0155e5dc0ce6a4166dd608ff11d5b
Marco Bulgarelli <marco.bulgarelli6@studio.unibo.it>
2025-02-05 10:34:17 +0100

    Added Inventory interface

--- src/main/java/fargoal/model/entity/player/api/Inventory.java
+++ src/main/java/fargoal/model/entity/player/api/Inventory.java
@@ -1,28 +1,174 @@
 package fargoal.model.entity.player.api;
 
+import java.util.List;
+
+/**
+ * Interface to manage player's items.
+ */
+
 public interface Inventory {
-    
-    Integer getHealingPotion();
+    /**
+     * Getter for the current number of available healing potions.
+     *  
+     * @return current number of available healing potions.
+     */
+    Integer getHealingPotions();
 
-    Integer getInvisibilitySpell();
+    /**
+     * Getter for the current number of available invisibility scrolls.
+     * 
+     * @return current number of available invisibility scrolls.
+     */
+    Integer getInvisibilityScrolls();
 
-    Integer getTeleportSpell();
+    /**
+     * Getter for the current number of available teleport scrolls.
+     * 
+     * @return current number of available teleport scrolls.
+     */
+    Integer getTeleportScrolls();
 
-    Integer getShieldSpell();
+    /**
+     * Getter for the current number of available shield scrolls.
+     * 
+     * @return current number of available shield scrolls.
+     */
+    Integer getShieldScrolls();
 
+    /**
+     * Getter for the current number of available drift scrolls.
+     * 
+     * @return current number of available drift scrolls.
+     */
+    Integer getDriftScrolls();
+
+    /**
+     * Getter for the current number of available light scrolls.
+     * 
+     * @return current number of available light scrolls.
+     */
+    Integer getLightScrolls();
+
+    /**
+     * Getter for the current number of available regeneration scrolls.
+     * 
+     * @return current number of available regeneration scrolls.
+     */
+    Integer getRegenerationScrolls();
+
+    /**
+     * Getter for the current number of available beacons.
+     * 
+     * @return current number of available beacons.
+     */
+    Integer getBeacons();
+
+    /**
+     * Getter for the current number of available magic sacks.
+     * 
+     * @return current number of available magic sacks.
+     */
+    Integer getMagicSacks();
+
+    /**
+     * Getter for the current number of available enchanted weapons.
+     * 
+     * @return current number of available enchaned weapons.
+     */
+    Integer getEnchantedWeapons();
+
+    /**
+     * Getter for the list containing the array list of maps the player has.
+     * 
+     * @return the list of floors for which the player has a map.
+     */
+    List<Integer> getListOfMaps();
+
+    /**
+     * Adds a single healing potion to player's inventory.
+     */
     void addHealingPotion();
 
+    /**
+     * Removes a single healing potion from player's inventory,
+     * if it is available.
+     */
     void removeHealingPotion();
 
-    void addInvisibilitySpell();
+    /**
+     * Adds a single invisibility scroll to player's inventory.
+     */
+    void addInvisibilityScroll();
 
-    void removeInvisibilitySpell();
+    /**
+     * Removes a single invisibility scroll from player's inventory,
+     * if it is available.
+     */
+    void removeInvisibilityScroll();
 
-    void addTeleportSpell();
+    /**
+     * Adds a single teleport scroll to player's inventory.
+     */
+    void addTeleportScroll();
 
-    void removeTeleportSpell();
+    /**
+     * Removes a single teleport scroll from player's inventory,
+     * if it is available.
+     */
+    void removeTeleportScroll();
 
-    void addShieldSpell();
+    /**
+     * Adds a single shield scroll to player's inventory.
+     */
+    void addShieldScroll();
 
-    void removeShieldSpell();
+    /**
+     * Removes a single shield scroll from player's inventory,
+     * if it is available.
+     */
+    void removeShieldScroll();
+
+    /**
+     * Adds a single beacon scroll to player's inventory.
+     */
+    void addBeacon();
+
+    /**
+     * Removes a single beacon scroll from player's inventory,
+     * if it is available.
+     */
+    void removeBeacon();
+
+    /**
+     * Adds a single drift scroll to player's inventory.
+     */
+    void addDriftScroll();
+
+    /**
+     * Removes a single beacon scroll from player's inventory,
+     * if it is available.
+     */
+    void removeDriftScroll();
+
+    /**
+     * Adds a single light scroll to player's inventory.
+     */
+    void addLightScroll();
+
+    /**
+     * Removes a single light scroll from player's inventory,
+     * if it is available.
+     */
+    void removeLightScroll();
+
+    /**
+     * Adds a single regeneration scroll to player's inventory.
+     */
+    void addRegenerationScroll();
+
+    /**
+     * Removes a single regeneration scroll from player's inventory,
+     * if it is available.
+     */
+    void removeRegenerationScroll();
 }
--- src/main/java/fargoal/model/entity/player/impl/InventoryImpl.java
+++ src/main/java/fargoal/model/entity/player/impl/InventoryImpl.java
@@ -1,83 +1,147 @@
 package fargoal.model.entity.player.impl;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import fargoal.model.entity.player.api.Inventory;
+
+/**
+ * Inventory class, it manages player's inventory
+ */
 
 public class InventoryImpl implements Inventory {
 
     private Integer healingPotions;
-    private Integer invisibilitySpells;
-    private Integer teleportSpells;
-    private Integer shieldSpells;
+    private Integer invisibilityScrolls;
+    private Integer teleportScrolls;
+    private Integer shieldScrolls;
+    private Integer driftScrolls;
+    private Integer lightScrolls;
+    private Integer regenerationScrolls;
+    private Integer beacons;
+    private Integer magicSacks;
+    private Integer enchantedWeapons;
+    private final List<Integer> listOfMaps = new ArrayList<>();
 
-    @Override
-    public Integer getHealingPotion() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getHealingPotion'");
+    /**
+     * Creates a new instance of Inventory, every number of available
+     * items is set to 0.
+     */
+    public InventoryImpl() {
+        this.healingPotions = 0;
+        this.invisibilityScrolls = 0;
+        this.teleportScrolls = 0;
+        this.shieldScrolls = 0;
+        this.beacons = 0;
+        this.lightScrolls = 0;
+        this.driftScrolls = 0;
+        this.regenerationScrolls = 0;
+        this.magicSacks = 0;
+        this.enchantedWeapons = 0;
     }
 
+    /**{@inheritDoc} */
     @Override
-    public Integer getInvisibilitySpell() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getInvisibilitySpell'");
+    public Integer getHealingPotions() {
+         return this.healingPotions;
     }
 
+    /**{@inheritDoc} */
     @Override
-    public Integer getTeleportSpell() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getTeleportSpell'");
+    public Integer getInvisibilityScrolls() {
+        return this.invisibilityScrolls;
     }
 
+    /**{@inheritDoc} */
     @Override
-    public Integer getShieldSpell() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getShieldSpell'");
+    public Integer getTeleportScrolls() {
+        return this.teleportScrolls;
     }
 
+    /**{@inheritDoc} */
+    @Override
+    public Integer getShieldScrolls() {
+        return this.shieldScrolls;
+    }
+
+    /**{@inheritDoc} */
+    @Override
+    public Integer getBeacons() {
+        return this.beacons;
+    }
+
+    /**{@inheritDoc} */
+    @Override
+    public void addBeacon() {
+        this.beacons ++;
+        
+    }
+
+    /**{@inheritDoc} */
     @Override
     public void addHealingPotion() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addHealingPotion'");
+        this.healingPotions ++;
+        
     }
 
+    /**{@inheritDoc} */
+    @Override
+    public void addInvisibilityScroll() {
+        this.invisibilityScrolls ++;
+        
+    }
+
+    /**{@inheritDoc} */
+    @Override
+    public void addShieldScroll() {
+        this.shieldScrolls ++;
+        
+    }
+
+    /**{@inheritDoc} */
+    @Override
+    public void addTeleportScroll() {
+        this.teleportScrolls ++;
+        
+    }
+
+    /**{@inheritDoc} */
+    @Override
+    public void removeBeaconScroll() {
+        if(this.getBeaconScrolls() > 0)
+            this.beaconScrolls --;
+    }
+
+    /**{@inheritDoc} */
     @Override
     public void removeHealingPotion() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeHealingPotion'");
+        if(this.getHealingPotions() > 0)
+            this.healingPotions --;
+        
     }
 
+    /**{@inheritDoc} */
     @Override
-    public void addInvisibilitySpell() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addInvisibilitySpell'");
+    public void removeInvisibilityScroll() {
+        if(this.getInvisibilityScrolls() > 0)
+            this.invisibilityScrolls --;
+        
     }
 
+    /**{@inheritDoc} */
     @Override
-    public void removeInvisibilitySpell() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeInvisibilitySpell'");
+    public void removeShieldScroll() {
+        if(this.getShieldScrolls() > 0)
+            this.shieldScrolls --;
+        
     }
 
+    /**{@inheritDoc} */
     @Override
-    public void addTeleportSpell() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addTeleportSpell'");
+    public void removeTeleportScroll() {
+        if(this.getTeleportScrolls() > 0)
+            this.teleportScrolls --;
+        
     }
-
-    @Override
-    public void removeTeleportSpell() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeTeleportSpell'");
-    }
-
-    @Override
-    public void addShieldSpell() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'addShieldSpell'");
-    }
-
-    @Override
-    public void removeShieldSpell() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'removeShieldSpell'");
-    }
+    
 }
