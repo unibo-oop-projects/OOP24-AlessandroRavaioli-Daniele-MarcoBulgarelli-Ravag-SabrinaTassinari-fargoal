e885283775952ad3ac5b946f46e8148c6dfd6f30
Marco Bulgarelli <marco.bulgarelli6@studio.unibo.it>
2025-02-05 16:20:37 +0100

    Added two more methods to PlayerImpl

--- src/main/java/fargoal/model/entity/player/impl/PlayerImpl.java
+++ src/main/java/fargoal/model/entity/player/impl/PlayerImpl.java
@@ -11,7 +11,7 @@ import fargoal.model.entity.player.api.Inventory;
 public class PlayerImpl implements Player {
 
     private static final int INITIAL_LEVEL = 1;
-    
+
     private Position position;
     private Integer level;
     private Integer experiencePoints;
@@ -25,7 +25,7 @@ public class PlayerImpl implements Player {
         this.position = null; //TODO
         this.level = INITIAL_LEVEL;
         this.experiencePoints = 0;
-        this.health = null; //TODO
+        this.health.setHealth(0); //TODO
         this.skill = null; //TODO
         this.gold = new GoldImpl();
         this.inventory = new InventoryImpl();
@@ -94,5 +94,35 @@ public class PlayerImpl implements Player {
     public void receiveDamage() {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'receiveDamage'");
+    }
+
+    /**
+     * This method manages the entire fight: it should check if player can and wants to flee
+     * it should manage player automatic turn and monster turn and return true if battle
+     * is won.
+     * 
+     * @param monster - The enemy monster.
+     * 
+     * @return true if player won the battle, false if they lose it.
+     */
+    private boolean battle(Monster monster) {
+        //TODO
+        throw new UnsupportedOperationException("Unimplemented method 'battle'");
+    }
+
+    /**
+     * This method is used to check wether the player character is dead or not.
+     * If the player has at least one healing potion and their health is not
+     * below a critical limit, then they should be able to automatically use 
+     * the healing potion and do not die.
+     * 
+     * @return true if player is dead, false otherwise.
+     */
+    private boolean isDead() {
+        if(inventory.getHealingPotions() == 0){
+            return this.health.getCurrentHealth() == 0;
+        } else {
+            return this.health.getCurrentHealth() < -5;
+        }
     }
 }
