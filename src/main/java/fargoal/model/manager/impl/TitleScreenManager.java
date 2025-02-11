888bbd3003bdd85437e6fc2ab2154c6cfb23bfa5
Alessandro Ravaioli <alessandro.ravaioli8@studio.unibo.it>
2025-02-11 10:48:48 +0100

    Added a title to the menu and updated the initializefloor method in floorManagerImpl

--- src/main/java/fargoal/model/manager/impl/TitleScreenManager.java
+++ src/main/java/fargoal/model/manager/impl/TitleScreenManager.java
@@ -5,17 +5,20 @@ import java.awt.Font;
 
 import fargoal.controller.input.api.KeyboardInputController;
 import fargoal.controller.input.api.MenuInputComponent;
+import fargoal.model.commons.Timer;
 import fargoal.model.core.GameEngine;
 import fargoal.model.manager.api.MenuManager;
 import fargoal.model.manager.api.SceneManager;
 import fargoal.view.api.Renderer;
 import fargoal.view.api.View;
 import fargoal.view.impl.SwingRendererMiddle;
+import fargoal.view.impl.SwingRendererTop;
 import fargoal.view.impl.SwingView;
 
 public class TitleScreenManager implements SceneManager, MenuManager {
 
     private static final int NUMBER_OF_OPTIONS = 2;
+    private static final long MILLIS_TO_WAIT = 200;
 
     private int selected;
     private final MenuInputComponent inputComp;
@@ -24,6 +27,7 @@ public class TitleScreenManager implements SceneManager, MenuManager {
     private Renderer title;
     private boolean timeToQuit;
     private boolean start;
+    private final Timer wait;
     
 
     public TitleScreenManager(GameEngine engine) {
@@ -32,6 +36,7 @@ public class TitleScreenManager implements SceneManager, MenuManager {
         this.ctrl = engine.getController();
         this.timeToQuit = false;
         this.start = false;
+        wait = new Timer();
         createRenderers(engine.getView());
     }
 
@@ -42,9 +47,12 @@ public class TitleScreenManager implements SceneManager, MenuManager {
         } else if (start) {
             setSceneManager(engine);
         }
-        this.inputComp.update(this, this.ctrl);
+        if (this.wait.updateTime(engine.getElapsedTime()) == 0) {
+            this.inputComp.update(this, this.ctrl);
+            this.wait.setTime(MILLIS_TO_WAIT);   
+        }
         this.menu.render();
-        // this.title.render();
+        this.title.render();
     }
 
     @Override
@@ -79,14 +87,21 @@ public class TitleScreenManager implements SceneManager, MenuManager {
         SwingView sView = (SwingView) view;
         this.menu = new SwingRendererMiddle(g2d -> {
                 g2d.setFont(new Font("Arial", Font.BOLD, sView.getFrame().getBounds().height * 3 / 110));
-                g2d.setColor((this.selected == 1) ? Color.cyan : Color.yellow);
+                g2d.setColor((this.selected == 1) ? Color.cyan : Color.red);
                 g2d.drawString("START GAME",
                         sView.getMapWidth() * 3/7,
                         sView.getMapHeight() * 1/7);
-                g2d.setColor((this.selected == 2) ? Color.cyan : Color.yellow);
+                g2d.setColor((this.selected == 2) ? Color.cyan : Color.red);
                 g2d.drawString("EXIT",
                         sView.getMapWidth() * 3/7,
                         sView.getMapHeight() * 3/7);
+        }, view);
+        this.title = new SwingRendererTop(g2d -> {
+            g2d.setFont(new Font("Arial", Font.BOLD, sView.getEventPanel().getBounds().height * 1/4));
+            g2d.setColor(Color.WHITE);
+            g2d.drawString("FARGOAL",
+                    sView.getMapWidth() * 3/7,
+                    sView.getEventPanel().getBounds().height / 2);
         }, view);
     }
 }
