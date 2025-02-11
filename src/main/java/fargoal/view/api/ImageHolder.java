86618ab14003167bfaa7f57e6ce9f66c074cf82c
Daniele <daniele.tramonti2@studio.unibo.it>
2025-02-11 14:41:22 +0100

    added javaDoc for every class in view.api

--- src/main/java/fargoal/view/api/ImageHolder.java
+++ src/main/java/fargoal/view/api/ImageHolder.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
-public class ImageHolder {
-    
+/**
+ * Class that has the objective to memorize in a static way
+ * images, to not have to read them at every frame.
+ */
+public final class ImageHolder {
     private static BufferedImage playerImage;
     private static BufferedImage chestImage;
     private static BufferedImage downstairsImage;
@@ -25,6 +28,8 @@ public class ImageHolder {
     private static BufferedImage upStairsImage;
     private static BufferedImage tileImage;
     private static BufferedImage templeImage;
+
+    private ImageHolder() { }
 
     static {
         try {
@@ -116,73 +121,157 @@ public class ImageHolder {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        
     }
 
+    /**
+     * Method that returns the image for the player.
+     * 
+     * @return - the image of the player
+     */
     public static BufferedImage player() {
         return playerImage;
     }
 
+    /**
+     * Method that returns the image for the chest.
+     * 
+     * @return - the image of the chest
+     */
     public static BufferedImage chest() {
         return chestImage;
     }
 
+    /**
+     * Method that returns the image for the downstairs.
+     * 
+     * @return - the image of the downstairs
+     */
     public static BufferedImage downstairs() {
         return downstairsImage;
     }
 
+    /**
+     * Method that returns the image for the hiddengold.
+     * 
+     * @return - the image of the hiddengold
+     */
     public static BufferedImage hiddenGold() {
         return hiddenGoldImage;
     }
 
+    /**
+     * Method that returns the image for the sack of gold.
+     * 
+     * @return - the image of the sack of gold
+     */
     public static BufferedImage sackOfGold() {
         return sackOfGoldImage;
     }
 
+    /**
+     * Method that returns the image for the sword.
+     * 
+     * @return - the image of the sword
+     */
     public static BufferedImage sword() {
         return swordImage;
     }
+
+    /**
+     * Method that returns the image for the assassin.
+     * 
+     * @return - the image of the assassin
+     */
     public static BufferedImage assassin() {
         return assassinImage;
     }
 
+    /**
+     * Method that returns the image for the barbarian.
+     * 
+     * @return - the image of the barbarian
+     */
     public static BufferedImage barbarian() {
         return barbarianImage;
     }
 
+    /**
+     * Method that returns the image for the mage.
+     * 
+     * @return - the image of the mage
+     */
     public static BufferedImage mage() {
         return mageImage;
     }
 
+    /**
+     * Method that returns the image for the monk.
+     * 
+     * @return - the image of the monk
+     */
     public static BufferedImage monk() {
         return monkImage;
     }
 
+    /**
+     * Method that returns the image for the rogue.
+     * 
+     * @return - the image of the rogue
+     */
     public static BufferedImage rogue() {
         return rogueImage;
     }
 
+    /**
+     * Method that returns the image for the spider.
+     * 
+     * @return - the image of the spider
+     */
     public static BufferedImage spider() {
         return spiderImage;
     }
 
+    /**
+     * Method that returns the image for the war lord.
+     * 
+     * @return - the image of the war lord
+     */
     public static BufferedImage warlord() {
         return warLordImage;
     }
 
-
+    /**
+     * Method that returns the image for the wall.
+     * 
+     * @return - the image of the wall
+     */
     public static BufferedImage wall() {
         return wallImage;
     }
 
+    /**
+     * Method that returns the image for the tile.
+     * 
+     * @return - the image of the tile
+     */
     public static BufferedImage tile() {
         return tileImage;
     }
 
+    /**
+     * Method that returns the image for the temple.
+     * 
+     * @return - the image of the temple
+     */
     public static BufferedImage temple() {
         return templeImage;
     }
 
+    /**
+     * Method that returns the image for the upstairs.
+     * 
+     * @return - the image of the upstairs
+     */
     public static BufferedImage upStair() {
         return upStairsImage;
     }
