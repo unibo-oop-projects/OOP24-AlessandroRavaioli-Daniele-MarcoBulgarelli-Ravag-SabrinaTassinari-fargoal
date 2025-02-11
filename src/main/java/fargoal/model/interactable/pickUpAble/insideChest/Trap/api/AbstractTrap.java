c8ddcd81982cc4beb4e44d1d8694560f0ee5686c
Daniele <daniele.tramonti2@studio.unibo.it>
2025-02-11 11:05:08 +0100

    added field mapLost in FoundTrapEvent

--- src/main/java/fargoal/model/interactable/pickUpAble/insideChest/Trap/api/AbstractTrap.java
+++ src/main/java/fargoal/model/interactable/pickUpAble/insideChest/Trap/api/AbstractTrap.java
@@ -2,6 +2,7 @@ package fargoal.model.interactable.pickUpAble.insideChest.Trap.api;
 
 import java.util.Random;
 
+import fargoal.model.events.impl.FoundTrapEvent;
 import fargoal.model.interactable.pickUpAble.insideChest.api.ChestItem;
 import fargoal.model.interactable.pickUpAble.insideChest.api.ChestItemType;
 import fargoal.model.manager.api.FloorManager;
@@ -17,11 +18,14 @@ public abstract class AbstractTrap implements ChestItem {
     /** {@inheritDoc} */
     @Override
     public void use(FloorManager floorManager) {
+        boolean mapLost = false;
         int chanceOfMapLost = new Random().nextInt(4);
         if (chanceOfMapLost == 0) {
+            mapLost = true;
             floorManager.getFloorMask().resetMask();
         }
         this.effect(floorManager);
+        floorManager.notifyFloorEvent(new FoundTrapEvent(this, mapLost));
     }
 
     public abstract void effect(FloorManager floorManager);
