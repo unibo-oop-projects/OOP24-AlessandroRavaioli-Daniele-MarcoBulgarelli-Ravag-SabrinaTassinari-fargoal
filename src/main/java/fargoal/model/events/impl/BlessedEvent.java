7d29a4046e037596a1c19e7294fa358509a9466a
Sabrina Tassinari <sabrina.tassinari@studio.unibo.it>
2025-02-13 10:33:39 +0100

    Fixed style

--- src/main/java/fargoal/model/events/impl/BlessedEvent.java
+++ src/main/java/fargoal/model/events/impl/BlessedEvent.java
@@ -7,7 +7,7 @@ import fargoal.model.events.api.FloorEvent;
  * is blessed in the temple.
  */
 public class BlessedEvent implements FloorEvent {
-    
+
     /**
      * This method returns the string that appears on screen when the player
      * is blessed in the temple.
